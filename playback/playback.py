5a13488c463c025a5c88d4f840ad393d9319d455
Muzamil <Muzamilb174@gmail.com>
2024-03-01 00:31:44 +0530

    Update playback.py

--- playback/playback.py
+++ playback/playback.py
@@ -2,8 +2,10 @@
 
 a ="This is CS50"
 print(a.replace(" ","..."))
+
 b ='This is our week on functions'
 print(b.replace(" ","..."))
+
 c ="Let's implement a function called hello"
 print(c.replace(" ","..."))
 
